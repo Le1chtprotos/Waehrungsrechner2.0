ad3459a9c7cb0aff2aa0a9fd5f0919d758f9e9ed
Max Schroeren <max.schreoren15@gmail.com>
2024-05-31 15:30:33 +0200

    baklaka

---
+++ script.js
@@ -0,0 +1,6 @@
+let api = 'https://v6.exchangerate-api.com/v6/b1575eb216e1fbcb9b4614dd/latest/USD'
+
+const fromDropDown = document.getElementById(fromselect);
+const toDropDown = document.getElementById(toselect);
+
+//Create dropdown from currencie
